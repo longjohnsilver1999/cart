29b512ddb0f5de855f4db2c3ad39853e144f00a4
sumitk-hub1999 <skatyal1999@gmail.com>
2022-07-25 07:23:01 +0530

    add props to cartitem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,10 @@
 import React from 'react';
 
 class CartItem extends React.Component{
-
-    constructor(){
-        super();
-        this.state={
-            
-            price:999,
-            title:'Mobile Phone',
-            qty:1,
-            img:''
-        }
+//we dont need the state
+   
         //this.increaseQuantity=this.increaseQuantity.bind(this);
-    }
+    
     increaseQuantity=()=>{
         console.log(this.state);
     //    this.setState({
@@ -37,7 +29,8 @@ class CartItem extends React.Component{
         })
     }
     render(){
-        const {price,title,qty}=this.state;
+        console.log("this.props",this.props);
+        const {price,title,qty}=this.props.product;
         return(
      <div className='cart-item'>
         <div className=''>
@@ -63,7 +56,7 @@ class CartItem extends React.Component{
 
                  <img 
                  alt='delete' 
-                 src='https://cdn-icons.flaticon.com/png/512/3405/premium/3405244.png?token=exp=1658464411~hmac=a80c2d5deb7ce30264caa6c58f286d1c' 
+                 src='https://cdn-icons.flaticon.com/png/512/3405/premium/3405244.png?token=exp=1658711910~hmac=3751081ba9bbd2c15c45f39e6d50b5ea' 
                  className='action-icons'></img>
              </div>
         </div>
