166578e04dc8769263b03409b321b7f590d527da
Sumit katyal <skatyal1999@gmail.com>
2024-05-30 05:20:00 +0000

    tushar dns ne aag laga di

--- demo/demo/src/main/java/com/example/demo/funController.java
+++ demo/demo/src/main/java/com/example/demo/funController.java
@@ -2,6 +2,9 @@
 package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+
 @RestController
 public class funController {
 @GetMapping("/")
@@ -17,5 +20,18 @@ public String sayHello1(){
 public String sayHello1q(){
     return "sign in mr tushar";
 }  
+@Value("${sumit.name}")
+private String sumitName;  
+
+
+@Value("${car.model}")
+private String carModel; 
+
+@GetMapping("/values")
+    public String getVals(){
+        return "abc"+ sumitName+"cars"+ carModel; 
 }
+}
+
+
 
