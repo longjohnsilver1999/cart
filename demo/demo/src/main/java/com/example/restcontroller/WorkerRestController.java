d3d08c94a461d598bd8665ad869729209c967380
Tushar <142966652+tusharrajput51220@users.noreply.github.com>
2024-06-12 12:24:34 +0000

    fgfg

--- demo/demo/src/main/java/com/example/restcontroller/WorkerRestController.java
+++ demo/demo/src/main/java/com/example/restcontroller/WorkerRestController.java
@@ -1,7 +1,9 @@
 package com.example.restcontroller;
 
 import com.example.workerentity.Worker;
+import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +13,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class WorkerRestController {
-
+    private List<Worker> theWorkers;
     // define endpoint for "/workers" - return a list of workers
-    @GetMapping("/workers")
-    public List<Worker> getWorkers() {
-
-        List<Worker> theWorkers = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+        theWorkers = new ArrayList<>();
 
         theWorkers.add(new Worker("Poornima", "Patel"));
         theWorkers.add(new Worker("Mario", "Rossi"));
         theWorkers.add(new Worker("Mary", "Smith"));
 
         return theWorkers;
+    }
+
+    // define endpoint for "/workers" - return a list of worker
+    @GetMapping("/workers")
+    public List<Worker> getWorkers() {
+        return theWorkers;
+    }
+
+    // define endpoint or "/workers/{workerId}" - return worker at index
+    @GetMapping("/workers/{workerId}")
+    public Worker getWorker(@PathVariable int workerId) {
+        return theWorkers.get(workerId);
     }
 }
